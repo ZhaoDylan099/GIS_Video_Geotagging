630a5c56f50e5449039a32bdc8e97fdf8bdfc070
ZhaoDylan099 <dylanzhao2004@gmail.com>
2025-12-15 18:09:24 -0500

    remove recordingsTable

--- RecordingsTable.sql
+++
@@ -1,8 +0,0 @@
-create table recordings (
-	audioID serial primary key,
-	time_s int not null,
-	transcript text not null,
-	audio bytea not null,
-	sample_rate int not null,
-	channels int not null
-)
